c92306ab3364436098e1581f4131756cda8570d6
anmol420 <anmolanand2k15@gmail.com>
2025-12-25 01:19:07 +0530

    added test for socket

--- backend/test/socket.test.ts
+++ backend/test/socket.test.ts
@@ -1,13 +1,36 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+console.log("Starting socket.io client test...");
 
-socket.on("connect", () => {
-  console.log("connected");
-  socket.emit("ping_test", { msg: "hello server" });
+const socket = io("http://localhost:3000", {
+  auth: {
+    token: Bun.env.TEST_JWT_TOKEN as string,
+  },
+  transports: ["websocket"],
 });
 
-socket.on("pong_test", data => {
-  console.log("pong received:", data);
-  socket.disconnect();
+socket.on("connect", () => {
+  console.log("Connected to server with ID:", socket.id);
+});
+
+socket.on("ready", () => {
+  console.log("Server ready, sending message...");
+  socket.emit("send_message", "Helloo again");
+});
+
+socket.on("new_message", (data) => {
+  console.log("New message received:", data);
+
+  setTimeout(() => {
+    socket.disconnect();
+    process.exit(0);
+  }, 500);
+});
+
+socket.on("disconnect", (reason) => {
+  console.log("Disconnected from server:", reason);
+});
+
+socket.on("connect_error", (err) => {
+  console.error("Connection error:", err.message);
 });
